4a175c6cc29573f9743d190baf2876af94d0afca
Neil <ndesai95@gmail.com>
2020-04-21 23:35:00 -0700

    Add clear videos to actions and videos reducer

--- frontend/actions/video_actions.js
+++ frontend/actions/video_actions.js
@@ -2,23 +2,28 @@ import * as VideoAPIUtil from '../util/video_api_util';
 
 export const RECEIVE_VIDEOS = 'RECEIVE_VIDEOS';
 export const RECEIVE_VIDEO = 'RECEIVE_VIDEO';
+export const CLEAR_VIDEOS = 'CLEAR_VIDEOS';
 
 export const receiveVideos = videos => ({
     type: RECEIVE_VIDEOS,
     videos
-})
+});
 
 export const receiveVideo = video => ({
     type: RECEIVE_VIDEO,
     video
-})
+});
+
+export const clearVideos = () => ({
+    type: CLEAR_VIDEOS
+});
 
 export const fetchVideos = () => dispatch => (
     VideoAPIUtil.fetchVideos()
     .then(videos => dispatch(receiveVideos(videos)))
-)
+);
 
 export const fetchVideo = id => dispatch => (
     VideoAPIUtil.fetchVideo(id)
     .then(video => dispatch(receiveVideo(video)))
-)
+);
