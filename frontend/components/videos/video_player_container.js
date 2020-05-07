f708b12afc57fe61dbe486cbe51c74c5c090afb2
Neil <ndesai95@gmail.com>
2020-05-06 23:51:10 -0700

    Create VideoPlayer and container

---
+++ frontend/components/videos/video_player_container.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchVideo } from '../../actions/video_actions';
+import VideoPlayer from './video_player';
+
+const msp = (state, ownProps) => {
+    let video;
+
+    if (ownProps.video === undefined) {
+        video = state.entities.videos[ownProps.match.params.mediaId];
+      } else {
+        video = ownProps.video;
+      }
+    
+      return ({
+        video
+    });
+};
+
+const mdp = dispatch => ({
+    fetchVideo: id => dispatch(fetchVideo(id))
+});
+  
+export default withRouter(connect(msp, mdp)(VideoPlayer));
